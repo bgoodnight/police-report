96672c1deb9032ed52a6132c2e3d450e85df0bc5
Goodnight <knz1@cdc.gov>
2018-02-15 10:12:48 -0500

    Modified to generate all reports

--- reports.R
+++ reports.R
@@ -5,12 +5,12 @@ library(rmarkdown)
 survey = read.csv('data.csv')
 
 #comment out line below to generate all reports
-survey <- survey[ which(survey$state=='GA' ), ]
+#survey <- survey[ which(survey$state=='GA' ), ]
 
 for (state in unique(survey$state)){
   print(paste("Compiling report for", state))
   data <- survey[ which(survey$state==state ), ]
   rmarkdown::render('template.Rmd',
-                    output_file =  paste(data[1,'state'], "_report", '_', Sys.Date(), ".docx", sep=''))
+                    output_file =  paste(data[1,'state'], "_report", '_', Sys.Date(), ".docx", sep=''), output_dir = 'reports')
 }
 
